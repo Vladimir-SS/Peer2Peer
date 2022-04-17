4fb3e4c95ac2aabeb162742da5a6095e800591c5
deliamanu <delia.08102001@gmail.com>
2022-04-18 00:01:16 +0300

    User and SharedFolderInfo added

--- ResidentApp/Classes/src/SharedFolderInfo.java
+++ ResidentApp/Classes/src/SharedFolderInfo.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SharedFolderInfo {
+    private String sharedFolderPath;
+    private List<FileObject> presentFiles = new ArrayList<>();
+
+    public SharedFolderInfo(String sharedFolderPath, List<FileObject> presentFiles) {
+        this.sharedFolderPath = sharedFolderPath;
+        this.presentFiles = presentFiles;
+    }
+
+    public boolean addFileObject(FileObject fileObject) {
+        if(!presentFiles.contains(fileObject)) {
+            presentFiles.add(fileObject);
+            return true;
+        }
+        return false;
+    }
+
+    public String getSharedFolderPath() {
+        return sharedFolderPath;
+    }
+
+    public void setSharedFolderPath(String sharedFolderPath) {
+        this.sharedFolderPath = sharedFolderPath;
+    }
+
+    public List<FileObject> getPresentFiles() {
+        return presentFiles;
+    }
+
+    public void setPresentFiles(List<FileObject> presentFiles) {
+        this.presentFiles = presentFiles;
+    }
+
+    @Override
+    public String toString() {
+        return "SharedFolderInfo{" +
+                "sharedFolderPath='" + sharedFolderPath + '\'' +
+                ", presentFiles=" + presentFiles +
+                '}';
+    }
 }
--- ResidentApp/Classes/src/User.java
+++ ResidentApp/Classes/src/User.java
@@ -1,2 +1,95 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String IPAddress;
+    private String name;
+    private List<User> connectedDevices = new ArrayList<>();
+    private SharedFolderInfo sharedFolderInfo;
+    private String sharedFolderPath;
+
+    public User(String IPAddress, String name, List<User> connectedDevices, SharedFolderInfo sharedFolderInfo, String sharedFolderPath) {
+        this.IPAddress = IPAddress;
+        this.name = name;
+        this.connectedDevices = connectedDevices;
+        this.sharedFolderInfo = sharedFolderInfo;
+        this.sharedFolderPath = sharedFolderPath;
+    }
+
+    public String addUserToConnect(User user) {
+        if(!connectedDevices.contains(user)) {
+            connectedDevices.add(user);
+            return user.name + "added";
+        } else {
+            return user.name + "already is connected";
+        }
+    }
+
+    public String getIPAddress() {
+        return IPAddress;
+    }
+
+    public void setIPAddress(String IPAddress) {
+        this.IPAddress = IPAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getConnectedDevices() {
+        return connectedDevices;
+    }
+
+    public void setConnectedDevices(List<User> connectedDevices) {
+        this.connectedDevices = connectedDevices;
+    }
+
+    public SharedFolderInfo getSharedFolderInfo() {
+        return sharedFolderInfo;
+    }
+
+    public void setSharedFolderInfo(SharedFolderInfo sharedFolderInfo) {
+        this.sharedFolderInfo = sharedFolderInfo;
+    }
+
+    public String getSharedFolderPath() {
+        return sharedFolderPath;
+    }
+
+    public void setSharedFolderPath(String sharedFolderPath) {
+        this.sharedFolderPath = sharedFolderPath;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "IPAddress='" + IPAddress + '\'' +
+                ", name='" + name + '\'' +
+                ", connectedDevices=" + connectedDevices +
+                ", sharedFolderInfo=" + sharedFolderInfo +
+                ", sharedFolderPath='" + sharedFolderPath + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) {
+            return true;
+        }
+        if(!(obj instanceof User)) {
+            return false;
+        }
+        User user = (User) obj;
+        return this.IPAddress.equals(((User) obj).IPAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode()*IPAddress.hashCode();
+    }
 }
