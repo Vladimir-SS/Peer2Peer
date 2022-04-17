4fc5076eac85b7bec3e53eb7697926335010688a
Ana Andrișan <ana.andrisan3@gmail.com>
2022-04-17 21:18:22 +0300

    basic

--- ResidentApp/Classes/src/FileInfo.java
+++ ResidentApp/Classes/src/FileInfo.java
@@ -1,2 +1,76 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class FileInfo {
+    private FileObject refersTo;
+    private List<User> sharedToUsers=new ArrayList<>();             //changed from User[]
+    private Date lastModified;
+    private Date lastSynchronized;
+
+    public FileInfo(FileObject refersTo) {
+        this.refersTo = refersTo;
+    }
+
+    public FileInfo(FileObject refersTo, List<User> sharedToUsers) {
+        this.refersTo = refersTo;
+        this.sharedToUsers = sharedToUsers;
+    }
+
+    public FileInfo(FileObject refersTo, List<User> sharedToUsers, Date lastModified, Date lastSynchronized) {
+        this.refersTo = refersTo;
+        this.sharedToUsers = sharedToUsers;
+        this.lastModified = lastModified;
+        this.lastSynchronized = lastSynchronized;
+    }
+
+    public void addSharedUser(User user){
+        this.sharedToUsers.add(user);
+    }
+
+    public void removeSharedUser(User user){
+        this.sharedToUsers.remove(user);
+    }
+
+    public FileObject getRefersTo() {
+        return refersTo;
+    }
+
+    public void setRefersTo(FileObject refersTo) {
+        this.refersTo = refersTo;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public Date getLastSynchronized() {
+        return lastSynchronized;
+    }
+
+    public void setLastSynchronized(Date lastSynchronized) {
+        this.lastSynchronized = lastSynchronized;
+    }
+
+    public List<User> getSharedToUsers() {
+        return sharedToUsers;
+    }
+
+    public void setSharedToUsers(List<User> sharedToUsers) {
+        this.sharedToUsers = sharedToUsers;
+    }
+
+    @Override
+    public String toString() {
+        return "FileInfo{" +
+                "refersTo=" + refersTo +
+                ", sharedToUsers=" + sharedToUsers +
+                ", lastModified=" + lastModified +
+                ", lastSynchronized=" + lastSynchronized +
+                '}';
+    }
 }
--- ResidentApp/Classes/src/FileObject.java
+++ ResidentApp/Classes/src/FileObject.java
@@ -1,2 +1,117 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileObject {
+    private String localPath;                       //mandatory
+    private String name;
+    private String sharedFolderPath;                //mandatory
+    private Integer id;                             //mandatory
+    private float size;                             //an int would have given only an approximate size
+    private FileInfo attachedInfo;
+
+    public FileObject(String localPath, String name, String sharedFolderPath, Integer id) {
+        this.localPath = localPath;
+        this.name = name;
+        this.sharedFolderPath = sharedFolderPath;
+        this.id = id;
+        this.size=getSize(localPath);
+    }
+
+    public FileObject(String localPath, String name, String sharedFolderPath, Integer id, FileInfo attachedInfo) {
+        this.localPath = localPath;
+        this.name = name;
+        this.sharedFolderPath = sharedFolderPath;
+        this.id = id;
+        this.attachedInfo = attachedInfo;
+        this.size=getSize(localPath);
+    }
+
+    public FileObject(String localPath, String sharedFolderPath, Integer id) {
+        this.name=getFileNameFromPath(localPath);
+        this.localPath = localPath;
+        this.sharedFolderPath = sharedFolderPath;
+        this.id = id;
+        this.size=getSize(localPath);
+    }
+
+    public FileObject(String localPath, String sharedFolderPath, Integer id, FileInfo attachedInfo) {
+        this.name=getFileNameFromPath(localPath);
+        this.localPath = localPath;
+        this.sharedFolderPath = sharedFolderPath;
+        this.id = id;
+        this.size=getSize(localPath);
+        this.attachedInfo = attachedInfo;
+    }
+
+    private String getFileNameFromPath(String path){
+        Path p= Paths.get(path);
+        return p.getFileName().toString();
+    }
+
+    public String getLocalPath() {
+        return localPath;
+    }
+
+    public void setLocalPath(String localPath) {
+        this.localPath = localPath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSharedFolderPath() {
+        return sharedFolderPath;
+    }
+
+    public void setSharedFolderPath(String sharedFolderPath) {
+        this.sharedFolderPath = sharedFolderPath;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public float getSize(String path){
+        File auxFile = new File(localPath);
+        float s=(float) auxFile.length() / 1024;
+        return s;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public FileInfo getAttachedInfo() {
+        return attachedInfo;
+    }
+
+    public void setAttachedInfo(FileInfo attachedInfo) {
+        this.attachedInfo = attachedInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "FileObject{" +
+                "localPath='" + localPath + '\'' +
+                ", name='" + name + '\'' +
+                ", sharedFolderPath='" + sharedFolderPath + '\'' +
+                ", id=" + id +
+                ", size=" + size +
+                ", attachedInfo=" + attachedInfo +
+                '}';
+    }
 }
