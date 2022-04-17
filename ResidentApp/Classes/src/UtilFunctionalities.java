9bdbaed3d9994257a6015c6cc4fae4ad92d82ac4
aanaolaru <79383164+aanaolaru@users.noreply.github.com>
2022-04-17 14:10:58 -0700

    Update UtilFunctionalities.java

--- ResidentApp/Classes/src/UtilFunctionalities.java
+++ ResidentApp/Classes/src/UtilFunctionalities.java
@@ -1,2 +1,11 @@
 public class UtilFunctionalities {
+    private void connectDeviceToNetwork(Device device){}
+    private void sync(){}
+    private void syncAtSpecifiedTime(){}
+    private void uploadFile(FileObject fileObject){}
+    private void viewFile(FileObject fileObject){}
+    private void login(String device, String password){}
+
+    public UtilFunctionalities() {}
 }
+
